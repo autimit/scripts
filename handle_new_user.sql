35b9d69bc9361fadd1b5ca5b6a8b1b6196b168ee
Antonio Anderson <hiroto.anderson@gmail.com>
2022-07-31 19:27:38 -0300

    Create handle_new_user.sql

---
+++ handle_new_user.sql
@@ -0,0 +1,23 @@
+-- pra cada novo usuário cadastrado, irá relacionar em uma tabela separada de usuários
+-- essa tabela foi criada para ter mais flexibilidade ao trabalhar com os dados dos usuários cadastros
+-- sem a necessidade de utilizar a auth.users
+
+-- cria a tabela users
+create table users ()
+  id uuid not null primary key,
+  email text
+);
+
+-- usa a procedure handle_new_user()
+create or replace function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.users(id, email)
+  values (new.id, new.email);
+  return new;
+end;
+$$ language plpgsql security definer;
+
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
